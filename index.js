3ec946b01b0fede4fdfd0a4f76310fd3050d81d2
Cybyl Jade <cybyljade@Cybyls-MacBook-Pro.local>
2022-06-28 23:39:23 -0600

    =(

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const PORT = process.env.PORT || 8080;
 const app = require('./server');
 
-app.listen(PORT, () => {
-  console.log(`The movie list server is running on port ${PORT}`);
-});
+app.listen(PORT)
