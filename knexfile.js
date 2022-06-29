28854532255a06e73b7168c50fc0640c8723dcfb
Cybyl Jade <cybyljade@Cybyls-MacBook-Pro.local>
2022-06-28 23:27:21 -0600

    second attempt to fix

--- knexfile.js
+++ knexfile.js
@@ -28,22 +28,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL+'?ssl=no-verify',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
@@ -55,4 +40,5 @@ module.exports = {
       directory: './seeds'
     }
   }
+  
 };
